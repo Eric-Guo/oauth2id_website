bf0500f59e8cc14a1ba5633801821431df35bf31
Eric-Guo <eric.guocz@gmail.com>
2019-02-26 16:21:42 +0800

    First custom home page and about page.

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "OAuth2 ID SSO", // <title>
+  shortSiteTitle: "OAuth2 ID - Affordable oauth2 SSO solution", // <title> ending for posts and pages
+  siteDescription: "OAuth2 ID is a standard affordable oauth2 SSO solution",
+  siteUrl: "https://www.oauth2id.com",
   // pathPrefix: "",
   siteImage: "preview.jpg",
-  siteLanguage: "en",
+  siteLanguage: "zh-CN",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Eric Guo",
+  authorTwitterAccount: "ecguo",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "OAuth2 ID",
+  headerSubTitle: "Affordable oauth2 SSO solution",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "OAuth2 ID - Affordable oauth2 SSO solution",
+  manifestShortName: "OAuth2ID", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -28,12 +28,12 @@ module.exports = {
   // Replace your email adress with md5-code.
   // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
   // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
-  gravatarImgMd5: "",
+  gravatarImgMd5: "https://en.gravatar.com/userimage/12217309/377db10e18b1dc1f363b7f5f0cb91964.jpeg",
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/Eric-Guo" },
+    { name: "twitter", url: "https://twitter.com/ecguo" },
+    { name: "facebook", url: "http://facebook.com/ecguo" }
   ]
 };
